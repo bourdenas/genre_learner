96a9cae2cb7613ae9aa2111ca1f6f40cd7e1249d
bourdenas <bourdenas@gmail.com>
2024-04-18 13:46:51 +0200

    Update training script's arguments and use keras format to save the model.

--- models/mobilenet_v2_images.py
+++ models/mobilenet_v2_images.py
@@ -9,7 +9,7 @@ import tensorflow.keras.layers as tfl
 
 
 def create_model(image_size, classes, learning_rate):
-    """
+    '''
     Create a model based on MobileNetV2 that can classify `classes`.
 
     Args:
@@ -19,7 +19,7 @@ def create_model(image_size, classes, learning_rate):
 
     Returns:
         model: A Keras model for classification based on MobileNetV2
-    """
+    '''
     input_shape = image_size + (3,)
     base_model = tf.keras.applications.MobileNetV2(input_shape=input_shape,
                                                    include_top=False,
@@ -44,7 +44,7 @@ def create_model(image_size, classes, learning_rate):
 
 
 def optimize_base_model(model, layer_to_start_tunning, learning_rate):
-    """
+    '''
     Create a model based on MobileNetV2 that can classify `classes`.
 
     Args:
@@ -54,10 +54,10 @@ def optimize_base_model(model, layer_to_start_tunning, learning_rate):
 
     Returns:
         model: A Keras model for classification based on MobileNetV2
-    """
+    '''
     base_model = model.layers[3]
     base_model.trainable = True
-    print("Number of layers in the base model: ", len(base_model.layers))
+    print('Number of layers in the base model: ', len(base_model.layers))
 
     for layer in base_model.layers[:layer_to_start_tunning]:
         layer.trainable = False
@@ -68,26 +68,28 @@ def optimize_base_model(model, layer_to_start_tunning, learning_rate):
     return model
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     parser = argparse.ArgumentParser(
-        description="Optimize the mobilenet_v2 model to predict game genre from single screenshots.")
+        description='Optimize the mobilenet_v2 model to predict game genre from single screenshots.')
     parser.add_argument(
-        "--train_dataset", help="Filepath with the dataset of images and classes to load for training.", default='dataset/')
+        '--dataset', help='Filepath with the dataset of images and classes to load for training.')
     parser.add_argument(
-        "--output_path", help="Filepath with the dataset of images and classes to load for training.", default='trained/')
+        '--output', help='Filepath with the dataset of images and classes to load for training.')
     parser.add_argument(
-        "--image_size", help="Image vertical size used as input for training. (default: 720)", type=int, default=720)
-    parser.add_argument("--image_aspect_ratio",
-                        help="Image aspect ratio. (default: 1.777)", type=float, default=1.7777777777)
-    parser.add_argument("--batch_size",
-                        help="Image training batch size. (default: 32)", type=float, default=32)
+        '--image_size', help='Image vertical size used as input for training. (default: 720)', type=int, default=720)
+    parser.add_argument(
+        '--image_aspect_ratio',
+        help='Image aspect ratio. (default: 1.777)', type=float, default=1.7777777777)
+    parser.add_argument(
+        '--batch_size',
+        help='Image training batch size. (default: 32)', type=float, default=32)
 
     args = parser.parse_args()
 
     image_size = (
         round(args.image_size * args.image_aspect_ratio), args.image_size)
 
-    (train_dataset, validation_dataset) = image_dataset_from_directory(args.train_dataset,
+    (train_dataset, validation_dataset) = image_dataset_from_directory(args.dataset,
                                                                        shuffle=True,
                                                                        batch_size=args.batch_size,
                                                                        image_size=image_size,
@@ -120,4 +122,4 @@ if __name__ == "__main__":
                              initial_epoch=history.epoch[-1],
                              validation_data=validation_dataset)
 
-    model.save(args.output_path)
+    model.save(args.output, save_format='keras')
