94783e6b44a4b172d8cc767d875339d5baecb8ea
bourdenas <bourdenas@gmail.com>
2024-04-18 14:32:41 +0200

    Add a simple prediction pipeline feeding images to a trained model.
    At the moment the model is constantly returning the same prediction regardless the input.

---
+++ models/predict_pipeline.py
@@ -0,0 +1,107 @@
+import os                                   # nopep8
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'    # nopep8
+
+import time
+import argparse
+import numpy as np
+import tensorflow as tf
+import tensorflow.keras.layers as tfl
+
+from collections import defaultdict
+from tensorflow.keras.preprocessing import image, image_dataset_from_directory
+
+
+def get_top_n(data, N):
+    '''
+    Get indices and values of the top N elements in a list.
+
+    Args:
+        data (list): A list of sortable elements.
+        N (int): An integer specifying the number of top elements to return.
+
+    Returns:
+        A tuple containing two lists:
+            - top_indices: Indices of the top N elements in the original order.
+            - top_values: Values of the top N elements.
+    '''
+    if not N or N < 0:
+        return [], []
+
+    sorted_data = sorted(enumerate(data), key=lambda x: x[1], reverse=True)
+    return sorted_data[:N]
+
+
+def collect_images(directory, extensions=('.jpg', '.jpeg', '.png')):
+    '''
+    Iterates through a directory structure and collects paths of images.
+
+    Args:
+        directory (str): The root directory to search within.
+        extensions (tuple, optional): A tuple of image file extensions to collect.
+                                      Defaults to ('.jpg', '.jpeg', '.png').
+
+    Returns:
+        list: A list of image file paths found within the directory structure.
+    '''
+
+    image_paths = defaultdict(list)
+    for root, _, files in os.walk(directory):
+        for file in files:
+            if file.lower().endswith(extensions):
+                image_paths[root].append(file)
+    return image_paths
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(
+        description='Annotate games with genres based on an input model using screenshot annotations.')
+    parser.add_argument(
+        '--dataset', help='Filepath with the dataset of images and classes to load for predictions.')
+    parser.add_argument(
+        '--model', help='Filepath to a model for annotating screenshots.')
+    args = parser.parse_args()
+
+    # TODO: That's a hack to load the dataset class names. Find a better way to retreive class labels.
+    (train_dataset, validation_dataset) = image_dataset_from_directory('dataset_single_genre/',
+                                                                       shuffle=True,
+                                                                       batch_size=32,
+                                                                       image_size=(
+                                                                           1280, 720),
+                                                                       validation_split=0.2,
+                                                                       subset='both',
+                                                                       seed=42)
+    class_names = train_dataset.class_names
+
+    model = tf.keras.models.load_model(args.model)
+
+    games = collect_images(args.dataset)
+    for (game, images) in games.items():
+        processed_images = []
+        for img in images:
+            img = image.load_img(os.path.join(game, img),
+                                 target_size=(1280, 720))
+            img_array = image.img_to_array(img)
+            img_array = tf.expand_dims(
+                img_array, axis=0)  # Add a batch dimension
+            processed_images.append(tf.keras.applications.mobilenet_v2.preprocess_input(
+                img_array))
+
+        img_processed = tf.concat(processed_images, axis=0)
+        print(img_processed.shape)
+        prediction = model.predict(img_processed, verbose=0)
+        prediction = prediction.tolist()
+        prediction = [get_top_n(pred, 3) for pred in prediction]
+
+        classification = defaultdict(int)
+        # `prediction` are the top N predictions for each image in the batch.
+        for pred in prediction:
+            (index, p) = pred[0]
+            classification[class_names[index]] += 1
+        print(f'{game} -> {classification}')
+        runner_up = defaultdict(int)
+        for pred in prediction:
+            (index, p) = pred[1]
+            runner_up[class_names[index]] += 1
+        print(f'\t\trunner up: {runner_up}')
+
+        time.sleep(1)
