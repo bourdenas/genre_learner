117eaf38971cce6e3b40462b6bcc3b12867b99f8
bourdenas <bourdenas@gmail.com>
2024-05-09 14:29:28 +0200

    Use sigmoid activation function for multi-label classification.
    The softmax was supressing too much the other labels. Also use binary crossentropy for loss function.

--- classifier/models/dense.py
+++ classifier/models/dense.py
@@ -14,11 +14,11 @@ def build(features, classes):
     inputs = tf.keras.Input(shape=(features,))
     X = tfl.Dense(2048, activation='relu')(inputs)
     X = tfl.Dropout(0.2)(X)
-    outputs = tfl.Dense(classes, activation='softmax')(X)
+    outputs = tfl.Dense(classes, activation='sigmoid')(X)
 
     model = tf.keras.Model(inputs, outputs)
     model.compile(optimizer=tf.keras.optimizers.Adam(learning_rate=0.0001),
                   #   loss='mean_squared_error',
-                  loss='categorical_crossentropy',
+                  loss='binary_crossentropy',
                   metrics=['accuracy'])
     return model
